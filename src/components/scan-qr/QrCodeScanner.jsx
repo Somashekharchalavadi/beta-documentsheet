a3af7fa38231a2213fc6fbf075ad49a1182167a9
Golu_singh <637golusingh@gmail.com>
2025-03-13 07:50:03 +0530

    update

--- src/components/scan-qr/QrCodeScanner.jsx
+++ src/components/scan-qr/QrCodeScanner.jsx
@@ -1,56 +1,28 @@
 import { useState } from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
-
-const ScanResultModal = ({ qrResult, closeModal }) => {
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-4 max-w-3xl w-full">
-        <div className="flex justify-end">
-          <button
-            onClick={closeModal}
-            className="text-lg font-bold text-black rounded-full border-2 w-8 h-8 flex justify-center items-center"
-          >
-            &times;
-          </button>
-        </div>
-
-        <h3 className="text-xl font-bold mb-4">Scanned Result:</h3>
-        <iframe src={qrResult} frameBorder="0" className="w-full md:h-96 h-full"></iframe>
-      </div>
-    </div>
-  );
-};
-
+import { useNavigate } from 'react-router-dom';
 const QrCodeScanner = () => {
-  const [qrResult, setQrResult] = useState(null);
   const [isScanning, setIsScanning] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-
+const navigate = useNavigate();
   const startScanner = () => {
     setIsScanning(true);
   };
 
   const stopScanner = () => {
     setIsScanning(false);
-    setQrResult(null); // Optionally clear the result when stopping the scanner
   };
 
   const handleScan = (result) => {
     if (result) {
-      setQrResult(result[0].rawValue);
+      navigate(result[0].rawValue);
       setIsScanning(false);
-      setShowModal(true);
+     
     }
   };
 
   const handleError = (err) => {
     setIsScanning(false);
     console.error('Scanning error:', err);
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-    setQrResult(null);
   };
 
   return (
@@ -93,24 +65,6 @@ const QrCodeScanner = () => {
         )}
       </div>
 
-      <div className="text-center mt-2">
-        {qrResult ? (
-          <div>
-            <h3 className="text-xl font-bold">Scanned Result:</h3>
-
-            <button
-              onClick={() => setShowModal(true)}
-              className="mt-2 text-blue-500 hover:underline"
-            >
-              View Full Result
-            </button>
-          </div>
-        ) : (
-          <p className="text-gray-500">No result yet. Please scan a QR code or barcode!</p>
-        )}
-
-        {showModal && <ScanResultModal qrResult={qrResult} closeModal={closeModal} />}
-      </div>
     </div>
   );
 };
