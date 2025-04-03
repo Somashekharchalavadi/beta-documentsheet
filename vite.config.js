f5102d6c4d9aa41afec1e657e4ba9f1733093d0f
Golu_singh <637golusingh@gmail.com>
2025-04-03 23:25:43 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '~': path.resolve(__dirname, 'node_modules'),
+    },
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        pure_getters: true,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+    outDir: 'build',
+    minify: 'terser',
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000,
+  },
 });
